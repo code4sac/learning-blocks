e0417c0f355bab21b956b2516c4f0ad6e57f5f48
Dan Fey <feydanm@gmail.com>
2022-03-09 19:53:53 -0800

    Adding transacript output script and clean data layout pdf

---
+++ Trancripts Output.py
@@ -0,0 +1,35 @@
+import requests
+import json
+API_HOST = "https://delnorte.asp.aeries.net/admin/api/v3/schools/520/students/grade/11"
+requestHeaders = {"formatType":"application/json", \
+					 "AERIES-CERT":"insert cert key here"}
+def reqpull():
+    request = requests.get(API_HOST, headers = requestHeaders)
+    r1= request.json()
+
+    
+ 
+    q=[]
+    a=[]
+    for q in r1:
+        a.append(q['PermanentID'])
+    print("The Permenant ID's for this grade level are")
+    print(a)
+    #if you want just the perm ID's delete below
+    for i  in a:
+        API_HOST1 = "https://delnorte.asp.aeries.net/admin/api/v3/schools/520/Transcript/"+x
+        requestHeaders1={"formatType":"application/json", \
+					 "AERIES-CERT":"insert vert key here"}
+        request1 = requests.get(API_HOST1, headers = requestHeaders1)
+        r2= request1.json()
+        print("Printing each transcript may take a while so i am going to make it better")
+        print(r2)
+        
+       
+
+
+
+
+
+#dont delete below this
+reqpull()
