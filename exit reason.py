b1df280e367be0916e10d1f15244bc75052d17f3
shwndea <35583425+shwndea@users.noreply.github.com>
2022-06-18 16:54:43 -0700

    Update exit reason.py

--- exit reason.py
+++ exit reason.py
@@ -1,47 +1,45 @@
-
-
-import numpy as np
+from typing import List
 import requests
-import json
-import pandas as pd
+import string
+import random
 import csv
 
-ID = ["Insert list of ID Number"]
-IDlen = len(ID)
-x = 0
-y = 0
-data = []
-d = {}
-df = pd.DataFrame(data=d)
+columns_to_include = [
+'StudentID',
+'LeaveDate',
+'ExitReasonCode'
+]
 
-while x < IDlen:
-    stdID = str(ID[x])
-    API_HOST = "https://aeries.gcccharters.org/Admin/api/v5/schools/815/enrollment/" + stdID
-    requestHeaders = {"formatType":"text/json", \
-					 "AERIES-CERT":"[INSERT API KEY]"}
-    request = requests.get(API_HOST, headers = requestHeaders)
-    requesttool = request.json()
-    data.append(requesttool)
-    x +=1
-with open('exit_reason.json', 'w') as exit_reason_json_file:
-    json.dump(data, exit_reason_json_file)
-with open('exit_reason.json') as json_file:
-    data = json.load(json_file)
-    jsonlen = len(data)
-    z=[]
-    z1=[]
-    z2=[]
-    while y < jsonlen:
-        z.append(data[y][1]['StudentID'])
-        z1.append(data[y][1]['LeaveDate'])
-        z2.append(data[y][1]['ExitReasonCode'])
-#data[y][1]['StudentID']
-#data[y][1]['LeaveDate']
-        y+=1
-df = pd.DataFrame(z)
-df.to_csv('StudentID(ExtReason).csv', header= ['StudentID'],index=False)
-data_new = pd.read_csv('filename.csv')
-data_new['EndDate'] = z1
-data_new.to_csv('StudentID&EndDate(ExtReason).csv')
-data_new['ExitReasonCode'] = z2
-data_new.to_csv('StudentID&EndDate&ExitReasonCode.csv') 
+def get_current_grades(
+    aeries_base_url: str,
+    aeries_api_token: str,
+    school_code: str,
+    student_ids: list[str]
+) -> str:
+  api_base = "%s/Admin/api/v5/schools/%s/enrollment" % aeries_base_url, school_code
+  request_headers = {"formatType":"text/json", "AERIES-CERT": aeries_api_token}
+  # random letters
+  ran_string = ''.join(random.choice(string.ascii_letters) for i in range(10))
+  file_path = 'Exit_Reason_%s.csv' % ran_string
+
+  with open(file_path, 'w', newline='') as current_grades_csv_file:
+    csv_writer = csv.writer(current_grades_csv_file)
+    csv_writer.writerow(columns_to_include) # header row in the csv
+    
+    for student_id in student_ids:
+      api_url = "%s/%s" % api_base, student_id
+      request_json = requests.get(api_url, headers = request_headers).json()
+      
+      for data in request_json:
+        filteredData = dict((k, data[k]) for k in columns_to_include if k in data)
+        x = len(filteredData['LeaveDate'])
+        csv_writer.writerow(filteredData.values())
+
+        for i in range(x):
+            csv_writer.writerow(filteredData[i][1]['StudentID'])
+            csv_writer.writerow(filteredData[i][1]['LeaveDate'])
+            csv_writer.writerow(filteredData[i][1]['ExitReasonCode'])
+
+  
+  return file_path
+
