5b40092db1ec1ba5fe0ae753e0b0aade29fe8f3f
shwndea <35583425+shwndea@users.noreply.github.com>
2023-02-24 15:30:23 -0800

    Update LearningBlocksFooter.vue

--- frontend/src/components/LearningBlocksFooter.vue
+++ frontend/src/components/LearningBlocksFooter.vue
@@ -14,9 +14,7 @@
       <v-col class="text-center mt-4" cols="12">
         {{ new Date().getFullYear() }} — <strong>Learning Blocks</strong>
       </v-col>
-      <v-col class="text-center mt-4" cols="12">
-        {{ new Date().getFullYear() }} — <strong>Learning Blocks</strong>
-      </v-col>
+   
     </v-row>
   </v-footer>
 
