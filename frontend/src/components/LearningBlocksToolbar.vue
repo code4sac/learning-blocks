b8f7e9face27d1b0d647f3c7fe547c721b17a3d4
shwndea <35583425+shwndea@users.noreply.github.com>
2023-02-24 15:50:46 -0800

    Update LearningBlocksToolbar.vue

--- frontend/src/components/LearningBlocksToolbar.vue
+++ frontend/src/components/LearningBlocksToolbar.vue
@@ -1,11 +1,5 @@
 <template>
-
-
-  const { createApp } = Vue
-  const { createVuetify } = Vuetify
-  const vuetify = createVuetify()
-
-  
+ 
   <v-app-bar :elevation="4" class="bg-blue-lighten-1">
     <template v-slot:prepend>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
@@ -15,13 +9,6 @@
     </v-app-bar-title>
     <v-spacer></v-spacer>
     <v-btn>Aries</v-btn>
-    const app = createApp({
-    template: '#app-template',
-    data: () => ({
-    selection: [],
-    items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
-    }),
-    }).use(vuetify).mount('#app')
     <v-col class="text-center mt-4" cols="12">
       {{ new Date().getFullYear() }} — <strong>Learning Blocks</strong>
     </v-col>
