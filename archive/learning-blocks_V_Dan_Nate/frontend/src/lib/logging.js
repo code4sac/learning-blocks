35d0acbf7e285c10f2c24dc78dc3b2d48c2081cc
shwndea <35583425+shwndea@users.noreply.github.com>
2023-10-14 12:51:48 -0700

    Delete archive directory

--- archive/learning-blocks_V_Dan_Nate/frontend/src/lib/logging.js
+++
@@ -1,9 +0,0 @@
-import logLevel from 'loglevel'
-
-export const log = logLevel.getLogger('trashai')
-log.setLevel('debug')
-// if (import.meta.env.DEV) {
-//     log.setLevel('debug')
-// } else {
-//     log.setLevel('info')
-// }
