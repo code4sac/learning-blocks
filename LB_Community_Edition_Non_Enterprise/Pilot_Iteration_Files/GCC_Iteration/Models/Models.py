7db97c41454a4a2eaa2a9d34e27d7c0b7e94f02d
shwndea <shwndea@aol.com>
2024-07-04 23:51:19 -0700

    Initialize Pilot w/files to fix up models

--- LB_Community_Edition_Non_Enterprise/Pilot_Iteration_Files/GCC_Iteration/Models/Models.py
+++ LB_Community_Edition_Non_Enterprise/Pilot_Iteration_Files/GCC_Iteration/Models/Models.py
@@ -1,74 +1,70 @@
-from sqlalchemy import Column, Integer, String, ARRAY, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, ARRAY, ForeignKey
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 from sqlalchemy.ext.declarative import declarative_base
-from typing import Optional, List
+from typing import List, Optional
 
 Base = declarative_base()
 
 class PeopleInDB(Base):
     __tablename__ = "people_in_db"
 
-    AnonymizedStudentID = Column(String, primary_key=True, index=True)
-    AnonymizedStudentNumber = Column(String, index=True, default=None)
-    AnonymizedCounselorNumber = Column(String, index=True, default=None)
-    AnonymizedHomeroomTeacherNumber = Column(String, index=True, default=None)
-    GraduationCohort = Column(String, index=True, default=None)
-    SchoolCode = Column(String, index=True, default=None)
-    Birthdate = Column(String, default=None)
-    EnabledUser = Column(String, default=None)
-    Role = Column(String, default=None)
-    Identifier = Column(String, default=None)
-    Grades = Column(ARRAY(String), default=None)
-    userIds = Column(String, index=True, default=None)
-    FamilyKey = Column(ARRAY(String), default=None)
-    AssociatedAccounts = Column(ARRAY(String), default=None)
-    SectionsIDs = Column(ARRAY(String), default=None)
-    GradebookIDs = Column(ARRAY(String), default=None)
-    DateLastModified = Column(String, default=None)
+    AnonymizedStudentID: Mapped[str] = mapped_column(String, primary_key=True, index=True)
+    AnonymizedStudentNumber: Mapped[Optional[str]] = mapped_column(String, index=True, default=None)
+    AnonymizedCounselorNumber: Mapped[Optional[str]] = mapped_column(String, index=True, default=None)
+    AnonymizedHomeroomTeacherNumber: Mapped[Optional[str]] = mapped_column(String, index=True, default=None)
+    GraduationCohort: Mapped[Optional[str]] = mapped_column(String, index=True, default=None)
+    SchoolCode: Mapped[Optional[str]] = mapped_column(String, ForeignKey('schools.school_code'), index=True, default=None)
+    Birthdate: Mapped[Optional[str]] = mapped_column(String, default=None)
+    EnabledUser: Mapped[Optional[str]] = mapped_column(String, default=None)
+    Role: Mapped[Optional[str]] = mapped_column(String, default=None)
+    Identifier: Mapped[Optional[str]] = mapped_column(String, ForeignKey('different_users.Identifier'), default=None)
+    Grades: Mapped[Optional[List[str]]] = mapped_column(ARRAY(String), default=None)
+    userIds: Mapped[Optional[str]] = mapped_column(String, index=True, default=None)
+    FamilyKey: Mapped[Optional[List[str]]] = mapped_column(ARRAY(String), default=None)
+    SectionsIDs: Mapped[Optional[List[str]]] = mapped_column(ARRAY(String), default=None)
+    GradebookIDs: Mapped[Optional[List[str]]] = mapped_column(ARRAY(String), default=None)
+    DateLastModified: Mapped[Optional[str]] = mapped_column(String, default=None)
     
     # Relationships using ForeignKey and back_populates
-    schools = relationship("SchoolRoster", back_populates="people")
-    different_users = relationship("UserAssociation", back_populates="people")
-    different_classes = relationship("ClassRoster", back_populates="people")
-    different_gradebooks = relationship("GradebookAssociation", back_populates="people")
+    schools: Mapped['SchoolRoster'] = relationship("SchoolRoster", back_populates="people")
+    AssociatedAccounts: Mapped[List['UserAssociation']] = relationship("UserAssociation", back_populates="people")
+    different_classes: Mapped[List['ClassRoster']] = relationship("ClassRoster", back_populates="people")
+    different_gradebooks: Mapped[List['GradebookAssociation']] = relationship("GradebookAssociation", back_populates="people")
 
 class SchoolRoster(Base):
     __tablename__ = "schools"
-
-    id = Column(Integer, primary_key=True, index=True)
-    school_code = Column(String, index=True, default=None)
-    people_id = Column(String, ForeignKey('people_in_db.SchoolCode'), default=None)
+    SchoolRosterID = Mapped[Optional[str]] = mapped_column(String, index=True, default=None)
+    school_code: Mapped[str] = mapped_column(String, primary_key=True, index=True)
+    name: Mapped[Optional[str]] = mapped_column(String, index=True, default=None)
 
     # Relationship to PeopleInDB using back_populates
-    people = relationship("PeopleInDB", back_populates="schools")
+    people: Mapped[List['PeopleInDB']] = relationship("PeopleInDB", back_populates="schools")
 
 class UserAssociation(Base):
     __tablename__ = "different_users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    family_key = Column(ARRAY(String), default=None)
-    associated_accounts = Column(ARRAY(String), default=None)
-    people_id = Column(String, ForeignKey('people_in_db.userIds'), default=None)
-
+    UserAssociationID = Mapped[Optional[str]] = mapped_column(String, index=True, default=None)
+    Identifier: Mapped[str] = mapped_column(String, ForeignKey('people_in_db.Identifier'), primary_key=True, index=True)
+    FamilyKey: Mapped[str] = mapped_column(String, ForeignKey('people_in_db.FamilyKey'), index=True) # associate with family key in people_in_db.FamilyKey
     # Relationship to PeopleInDB using back_populates
-    people = relationship("PeopleInDB", back_populates="different_users")
+    AssociatedAccounts: Mapped['PeopleInDB'] = relationship("PeopleInDB", back_populates="AssociatedAccounts")
+
 
 class ClassRoster(Base):
     __tablename__ = "different_classes"
-
-    id = Column(Integer, primary_key=True, index=True)
-    sections_ids = Column(ARRAY(String), default=None)
-    people_id = Column(String, ForeignKey('people_in_db.SectionsIDs'), default=None)
+    ClassRosterID = Mapped[Optional[str]] = mapped_column(String, index=True, default=None)
+    Identifier: Mapped[str] = mapped_column(String, ForeignKey('people_in_db.Identifier'), primary_key=True, index=True)
+    SectionsIDs: Mapped[Optional[List[str]]] = mapped_column(ARRAY(String), ForeignKey('people_in_db.SectionsIDs'),  index=True, default=None)
 
     # Relationship to PeopleInDB using back_populates
-    people = relationship("PeopleInDB", back_populates="different_classes")
+    people: Mapped['PeopleInDB'] = relationship("PeopleInDB", back_populates="different_classes")
+
 
 class GradebookAssociation(Base):
     __tablename__ = "different_gradebooks"
 
-    id = Column(Integer, primary_key=True, index=True)
-    gradebook_ids = Column(ARRAY(String), default=None)
-    people_id = Column(String, ForeignKey('people_in_db.GradebookIDs'), default=None)
+    GradebookAssociationID = Mapped[Optional[str]] = mapped_column(String, index=True, default=None)
+    Identifier: Mapped[str] = mapped_column(String, ForeignKey('people_in_db.Identifier'), primary_key=True, index=True)
+    GradebookIDs: Mapped[Optional[List[str]]] = mapped_column(ARRAY(String), ForeignKey('people_in_db.GradebookIDs'),  index=True, default=None)
 
     # Relationship to PeopleInDB using back_populates
     people = relationship("PeopleInDB", back_populates="different_gradebooks")
