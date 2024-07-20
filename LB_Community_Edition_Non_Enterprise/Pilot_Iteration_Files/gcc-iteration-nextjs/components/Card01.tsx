aeef0c50d62800e066b828a7194e576581939e3e
natebass <nw3455@gmail.com>
2024-07-19 22:27:20 -0700

    Add components folder from lb_frontend02 Azure website.

---
+++ LB_Community_Edition_Non_Enterprise/Pilot_Iteration_Files/gcc-iteration-nextjs/components/Card01.tsx
@@ -0,0 +1,28 @@
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+function Card01() {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="/demo_03.png" />
+      <Card.Body>
+        <Card.Title>Powerschool Mtss & RTI Scheduler</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Card01;
