54cb4261814fe62e12fc09832bbf3ddd79f4ec27
natebass <nw3455@gmail.com>
2024-07-19 22:27:20 -0700

    Add components folder from lb_frontend02 Azure website.

---
+++ LB_Community_Edition_Non_Enterprise/Pilot_Iteration_Files/gcc-iteration-nextjs/components/AppFooter.tsx
@@ -0,0 +1,16 @@
+import Container from 'react-bootstrap/Container';
+
+function AppNavBar() {
+  return (
+    <>
+      <Container>
+
+        <div>
+          <span style={{ float: 'right' }}><img src="icon_info.svg" /> Learning Blocks Enterprise Demo v0.0.1-microsoft-azure.1</span>
+        </div>
+      </Container>
+    </>
+  );
+}
+
+export default AppNavBar;
