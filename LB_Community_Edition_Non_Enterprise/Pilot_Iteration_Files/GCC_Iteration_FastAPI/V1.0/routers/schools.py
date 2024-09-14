10572bcad69535b1740f40ab6aff64ccd78ca202
shwndea <shwndea@aol.com>
2024-09-13 22:08:19 -0700

    modified:   LB_Community_Edition_Non_Enterprise/Pilot_Iteration_Files/GCC_Iteration_FastAPI/V1.0/routers/schools.py

--- LB_Community_Edition_Non_Enterprise/Pilot_Iteration_Files/GCC_Iteration_FastAPI/V1.0/routers/schools.py
+++ LB_Community_Edition_Non_Enterprise/Pilot_Iteration_Files/GCC_Iteration_FastAPI/V1.0/routers/schools.py
@@ -54,7 +54,7 @@ def create_school(school: SchoolsInDBCreate, db: Session = Depends(get_db)):
         db.rollback()
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
     
-    
+
 @router.get("/schools/{school_id}", response_model=SchoolsInDBResponse)
 def read_school(school_id: int, db: Session = Depends(get_db)):
     school = db.query(SchoolsInDB).filter(SchoolsInDB.ID == school_id).first()
@@ -71,12 +71,13 @@ def update_school(school_id: int, school: SchoolsInDBCreate, db: Session = Depen
         raise HTTPException(status_code=404, detail="School not found")
     
     try:
-        existing_school.school_code = school.school_code
-        existing_school.school_name = school.school_name
-        existing_school.address = school.address
-        existing_school.city = school.city
-        existing_school.state = school.state
-        existing_school.zip_code = school.zip_code
+        existing_school.SchoolCode = school.SchoolCode
+        existing_school.SchoolName = school.SchoolName
+        existing_school.Address = school.Address
+        existing_school.City = school.City
+        existing_school.State = school.State
+        existing_school.ZipCode = school.ZipCode
+        existing_school.MetaData = json.dumps(school.MetaData.model_dump()) if school.MetaData else None  # Replace dict() with model_dump()
 
         db.commit()
         db.refresh(existing_school)
